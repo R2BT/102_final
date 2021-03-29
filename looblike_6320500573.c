18804552faec368b9f22396a467372769b2f299a
R2BT <theeraphat.a@ku.th>
2021-03-29 18:30:47 +0700

    2

--- looblike_6320500573.c
+++ looblike_6320500573.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,max,j;
+    int n,i,max,j,sum=0;
     scanf("%d",&n);
-    int like[n];
-    for(i=0;i<n;i++)
+    int like[n],comlike[n];
+    for(i=0; i<n; i++)
     {
         scanf("%d",&like[i]);
     }
@@ -12,19 +12,48 @@ int main()
     i=0;
     while(j<n)
     {
-        if(like[i]>like[j])
+        if(like[i]>=like[j])
         {
-            max=i;
+            max=like[i];
             j++;
         }
-
-        else
+        else if(like[i]<=like[j])
         {
-            max=j;
+            max=like[j];
             i++;
             j++;
         }
     }
-    printf("%d",max+1);
+    i=0;
+    while(i<n)
+    {
+        if(sum==2)
+        {
+            break;
+        }
+        else
+        {
+            if(max==like[i])
+            {
+                sum++;
+            }
+        }
+        i++;
+    }
+    if(sum!=2)
+    {
+        for(i=0; i<n; i++)
+        {
+            if(max==like[i])
+            {
+                printf("%d",i+1);
+            }
+        }
+    }
+    else
+    {
+        printf("1");
+    }
+
     return 0;
 }
